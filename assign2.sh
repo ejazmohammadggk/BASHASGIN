1dee8fc0c4eaec946cacfb2933457253a75ec370
ejazmohammadggk <32570711+ejazmohammadggk@users.noreply.github.com>
2017-10-21 17:56:47 +0530

    Update assign2.sh

--- assign2.sh
+++ assign2.sh
@@ -1,1 +1,17 @@
-useradd
+
+$uname=$(sed '1!d' inputs);
+$gname=$(sed '3!d' inputs);
+(
+sed '2!d' inputs;
+sed '2!d' inputs;
+echo;
+echo;
+echo;
+echo;
+echo;
+echo y
+)
+|
+adduser $uname;
+groupadd $gname
+usermod -aG sudo $uname
