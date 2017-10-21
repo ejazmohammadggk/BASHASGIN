51311dbb37edf6fb05bd69bfbb29561e3e4e1d80
ejazmohammadggk <32570711+ejazmohammadggk@users.noreply.github.com>
2017-10-21 17:58:09 +0530

    Update assign2.sh

--- assign2.sh
+++ assign2.sh
@@ -1,9 +1,9 @@
 
-$uname=$(sed '1!d' inputs);
-$gname=$(sed '3!d' inputs);
+$uname=$(sed '1!d' inputs2);
+$gname=$(sed '3!d' inputs2);
 (
-sed '2!d' inputs;
-sed '2!d' inputs;
+sed '2!d' inputs2;
+sed '2!d' inputs2;
 echo;
 echo;
 echo;
