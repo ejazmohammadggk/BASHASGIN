9e73c197fe4e43341eabd5b3e8cc377eaf0bb7d8
Ubuntu <ubuntu@ip-172-31-30-145.us-west-2.compute.internal>
2017-10-21 09:38:11 +0000

    1sth assigment

---
+++ assign1.sh
@@ -0,0 +1,14 @@
+
+(sed '1!d' inputs;
+sed '2!d' inputs;
+sed '3!d' inputs;
+sed '4!d' inputs;
+sed '5!d' inputs;
+sed '6!d' inputs;
+sed '7!d' inputs;
+sed '8!d' inputs;
+sed '9!d' inputs;
+sed '10!d' inputs;
+sed '11!d' inputs;
+ )|
+fdisk /dev/xvdf
